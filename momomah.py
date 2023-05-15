3b04b8f74849ea11a6135dcec7151351f48b9b72
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-15 23:07:48 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -1326,7 +1326,7 @@ def ffb6(ids,names,passlist):
 		
 	
 def menu_apikey():  
-  myid=uuid.uuid4().hex[:10].upper()
+  myid=uuid.uuid4.uuid4().hex[:10].upper()
   ak="GSXD" 
   server = requests.get('https://github.com/mdtasin123/Approval.txt/blob/main/Approval.txt').text
   
@@ -1354,7 +1354,7 @@ def menu_apikey():
   print("\033[1;37m--------------------------------------------------")
   print("")
   print("\033[1;37m ╔═══════════════════════════════════════════════╗ ")
-  print("\x1b[1;92m       YOUR  KEY : "+ak+myid)
+  print("\x1b[1;92m       YOUR  KEY : "+ak+myid+myid)
   print("\033[1;37m ╚═══════════════════════════════════════════════╝ ")
   print("")
   print("\033[1;37m--------------------------------------------------")
