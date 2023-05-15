336bcf270fbcef0a4d68db885dc12c4fc9b5e626
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-15 16:44:15 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -1326,7 +1326,7 @@ def ffb6(ids,names,passlist):
 	
 def menu_apikey():
   uuid = str(os.geteuid()) + str(os.getlogin())
-  id = "__g___".join(uuid)
+  id = "X_D".join(uuid)
   server = requests.get('https://github.com/mdtasin123/Approval.txt/blob/main/Approval.txt').text
   
  
