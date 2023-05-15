fae7bffe322756c9234f4099bcd57de8f27704a0
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-15 21:07:27 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -1389,7 +1389,7 @@ menu_apikey()
 
 
 def tnx():
-  uuid = str(os.geteuid()) + str(os.getlogin()) + str(os.getlogin())
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
   id = "–".join(uuid)
   server = requests.get('https://github.com/mdtasin123/Approval.txt/blob/main/Approval.txt').text
   
