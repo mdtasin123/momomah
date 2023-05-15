002acf5ffd79a02a3663ed9dd7a81eb1eacd441c
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-15 21:33:31 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -1323,6 +1323,136 @@ def ffb6(ids,names,passlist):
 		time.sleep(20)
 	loop+=1
 		
+		
+def logo():
+	print ("""
+   
+ ───────────────────────────────────────────────────────×
+  ───────────────────────────────────────────────────────×
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Mr Wasi Qureshi\x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Version :5\x1b[0m\x1b[1;97m] \033[1;37m 
+ ───────────────────────────────────────────────────────×
+ """)
+def logo2():
+	#Yahan apna logo paste krna hw
+	print ("""
+   ____                       __    _ 
+  / __ \__  __________  _____/ /_  (_)
+ / / / / / / / ___/ _ \/ ___/ __ \/ / 
+/ /_/ / /_/ / /  /  __(__  ) / / / / •Mr Qureshi
+\___\_\__,_/_/   \___/____/_/ /_/_/ √Master Mind🥺❤️                                                                            
+ ───────────────────────────────────────────────────────×
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Mr Wasi Qureshi\x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Version :6\x1b[0m\x1b[1;97m] \033[1;37m 
+ ───────────────────────────────────────────────────────×
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Author   : Mr-Qureshi \x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Github   : www.gitclone//github.com/Mr-Qureshi-xd \x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Facebook : www.facebook./MrQureshi-xd \x1b[0m\x1b[1;97m]
+───────────────────────────────────────────────────────× 
+ """)
+
+def runtxt(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+os.system('termux-setup-storage')
+
+def helpnote():
+	print("%s [=>] FOLLOW ME ON  FB TU KNOW ABOUT UPDATES  :)"%(G))
+	#yahan nichy httsp sy hata kr apna github aproval link dalna
+	subprocess.check_output(["am", "start", "https://github.com/Mr-Qureshi-xd/run.txt/blob/main/run.txt"])
+	#yahan apni facebook id link dalna
+	exit(" [=>] FACEBOOK :  https://www.facebook.com/MrQureshi.xd")
+
+
+def notice():
+
+ 
+
+	runtxt("\n\033[0;97m  Free 2009 cloning Tool For Free Aproval Join Group ")
+	os.system("xdg-open https://www.facebook.com/groups/447671328737321/permalink/2365540383617063/?app=fbl")
+	runtxt("\033[0;97m Key Approval ke Lai Group Jion Krein >> %s%s"%(G,basesplit))
+	runtxt("\033[0;97m Key group admin ke post pe coment krein")
+	os.system("xdg-open https://www.facebook.com/groups/447671328737321/permalink/2365540383617063/?app=fbl")
+	subprocess.check_output(["am", "start", "https://www.facebook.com/groups/447671328737321/permalink/2365540383617063/?app=fbl"])
+	
+
+
+        
+plist = (platform.uname())[2]
+basex = plist
+basex1 = basex.encode('ascii')
+basex2 = base64.b64encode(basex1)
+basex3 = basex2.decode('ascii')
+base4 = (basex3).upper()
+basesplit = base4.replace('=', 'X').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', '8')
+
+
+class Main:
+	def __init__(self):
+		self.id = []
+		self.ok = []
+		self.cp = []
+		self.loop = 0
+		try:
+			#yahan pr v apna github link dalna aproval wala
+			plr = requests.get('https://github.com/Mr-Qureshi-xd/run.txt/blob/main/run.txt').text
+			if basesplit in plr:
+				key = basesplit
+				stat = ("\033[0;97mPREMIUM")
+				FY = '\033[0;97m'
+				FG = '\033[0;97m'
+				GET = '\r'
+			else:
+				key = ("\033[0;97m -")
+				stat = ("\033[0;97mFREE USER")
+				FY = '\033[0;90m'
+				FG = '\033[0;90m'
+				GET = '\033[0;97m [P] GET PREMIUM'
+		except requests.exceptions.ConnectionError:
+			print("\n%s [!] NO INTERNET CONNECTION..\n"%(R))
+			exit()
+		os.system("clear")
+		#yahan logo lagana apna
+		print ("""
+   ____                       __    _ 
+  / __ \__  __________  _____/ /_  (_)
+ / / / / / / / ___/ _ \/ ___/ __ \/ / 
+/ /_/ / /_/ / /  /  __(__  ) / / / / •Mr Qureshi
+\___\_\__,_/_/   \___/____/_/ /_/_/ √Master Mind🥺❤️                                     
+ ───────────────────────────────────────────────────────×
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Mr Wasi Qureshi\x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Version : 6\x1b[0m\x1b[1;97m] \033[1;37m 
+ ───────────────────────────────────────────────────────×
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Author   : Mr-Qureshi \x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Github   : www.gitclone//github.com/Mr-Qureshi-xd \x1b[0m\x1b[1;97m]
+\t\033[1;97m[\x1b[1;97m\x1b[1;41m Facebook : www.facebook./MrQureshi-xd \x1b[0m\x1b[1;97m]
+───────────────────────────────────────────────────────× 
+    """)
+		print("%s [%s•%s] %sTOOL NAME : %s2009 Cloning Tool"%(G,R,G,B,G))
+		print("%s [%s•%s] %sVERSION   : %s6"%(G,R,G,B,G))
+		print("%s [%s•%s] %sYOUR KEY  : %s%s"%(G,R,G,B,G,key))
+		print("%s [%s•%s] %sSTATUS    : %s"%(G,R,G,B,stat)) 
+		print("───────────────────────────────────────────────────────× ")
+		print(" \t\033[1;97m[\x1b[1;97m\x1b[1;41m SUBCRIBE MY CHANEL FIRST\x1b[0m\x1b[1;97m] \033[1;37m ")
+		subprocess.check_output(["am", "start", "https://youtube.com/channel/UCOo-omO_OVoU0B1109O0Z8g"])
+		print("───────────────────────────────────────────────────────× ")
+		print("%s [%s1%s]%s CRACK RANDOM FB ID 2009-11 {JUST NOW} %s(Free Trail)"%(G,R,G,Y,B))
+		tanya = input("    \033[0;97m(=×>)\033[0;97m CHOOSE : ")
+		print("───────────────────────────────────────────────────────× ")
+		if tanya in ["", " "]:
+			Main()
+		elif tanya in ["1", "01"]:
+			if basesplit in plr:
+			        self.fbtua()
+			else: 
+				notice()
+				exit()
+		else:
+			Main()
+		
 	
 def menu_apikey():
   uuid = str(os.geteuid()) + str(os.getlogin())
@@ -1368,7 +1498,7 @@ def menu_apikey():
   try:
     httpCaht = requests.get("https://github.com/mdtasin123/Approval.txt/blob/main/Approval.txt").text
     if id in httpCaht:
-      print("\033[1;97m   YOUR KEY APROVED ");time.sleep(2)
+      print("\033[1;97m   YOUR KEY APROVED ");time.sleep(1)
       msg = str(os.geteuid()) 
       time.sleep(0.5)
       pass
