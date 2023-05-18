3997b6af4264aeafb013ee50e9708c5863226c86
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-18 12:55:46 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -633,7 +633,6 @@ def menu():
 		linex()
 		plist = []
 		try:
-		plist = open(mthd,'r').read().splitlines()
 		except Method Not Found Error:
 			print(' Method Not Found Error')
 			time.sleep(1)
